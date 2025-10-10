c8d8d6833a2e855a433806e184ad7f25c36dc8fc
Henrique Braz <122986023+HenriqueBrazWork@users.noreply.github.com>
2025-10-10 22:39:39 +0100

    Update auth.py

--- AiiT-Lab/AiiT_app/backend/auth.py
+++ AiiT-Lab/AiiT_app/backend/auth.py
@@ -1,1 +1,13 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+import jwt
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+SECRET_KEY = "supersecretkey"
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        return {"email": payload["sub"]}
+    except:
+        raise HTTPException(status_code=401, detail="Token inválido")
